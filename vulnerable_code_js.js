86414d94b7d79c62afd1b2ce2b4cff05f7699bce
AxelHerrera4 <93292952+AxelHerrera4@users.noreply.github.com>
2025-12-18 09:40:16 -0500

    corregido

--- vulnerable_code_js.js
+++ vulnerable_code_js.js
@@ -11,17 +11,4 @@ function ejecutarOperacion(operacion, a, b) {
     }
 
     return operacionesPermitidas[operacion](a, b);
-}function ejecutarOperacion(operacion, a, b) {
-    const operacionesPermitidas = {
-        suma: (x, y) => x + y,
-        resta: (x, y) => x - y,
-        multiplicacion: (x, y) => x * y,
-        division: (x, y) => y !== 0 ? x / y : null
-    };
-
-    if (!operacionesPermitidas[operacion]) {
-        throw new Error("Operación no permitida");
-    }
-
-    return operacionesPermitidas[operacion](a, b);
 }
